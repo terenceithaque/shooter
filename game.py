c0eb3392fd8ecbb12aa8876e0445a106d2f07f9e
terenceithaque <terence.lethierry@protonmail.com>
2022-11-12 16:58:44 +0100

    Déplacements et création du personnage (1)

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+from player import Player
+
+# creer une seconde classe qui va representer notre jeu
+class Game:
+
+  def __init__(self):
+    # generer notre joueur
+    self.player = Player()
+    self.pressed = {}
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pygame
+
+from game import Game
+from player import Player
+
 pygame.init()
+
+
+
+    
+
+
+
+
+
 
 # Generer la fenetre de notre jeu
 pygame.display.set_caption("Comet fall Game")
@@ -8,13 +21,30 @@ screen = pygame.display.set_mode((1080, 720))
 # importer et charger l'image de notre jeu
 background = pygame.image.load("assets/bg.jpg")
 
+# charger notre jeu
+game = Game()
+
+
 running = True
 
 # Boucle tant que cette condition est vrai
 while running:
 
-  # appliquer l'arriere plan de notre jeu
+  # appliquer la  fenetre du jeu
   screen.blit(background, (0, -200))
+
+
+  
+# appliquer l'image de mon joueur
+  screen.blit(game.player.image, game.player.rect)
+
+
+# verifier si le joueur souhaite aller à gauche ou à droite
+  if game.pressed.get(pygame.K_RIGHT):
+    game.player.move_right()
+
+  elif game.pressed.get(pygame.K_LEFT):
+    game.player.move_left()   
 
   # mettre à jour l'ecran
   pygame.display.flip()
@@ -26,3 +56,11 @@ while running:
       running = False
       pygame.quit()
       print("Fermeture du jeu")
+    # detecter si un joueur lache une touche du clavier
+    elif event.type == pygame.KEYDOWN:
+      game.pressed[event.key] = True
+    elif event.type == pygame.KEYUP:
+      game.pressed[event.key] = False
+
+
+
